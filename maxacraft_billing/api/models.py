bddac7c50b7c15b8de4d10e840d071ceebb8a8dd
dmitriy <dmitriy250300@mail.ru>
2023-08-01 15:08:25 +0300

    01.08.2023

--- maxacraft_billing/api/models.py
+++ maxacraft_billing/api/models.py
@@ -26,23 +26,44 @@ class UserAccount(models.Model):
         return str(self.id)
 
 
-class UserAccount(models.Model):
-    ACCOUNT_TYPE_CHOICES = [
-        ('minecraft-server', 'Minecraft Server'),
-        ('web', 'Web'),
+class Transaction(models.Model):
+    TRADE_TYPE_CHOICES = [
+        ('auction', 'Auction'),
+        ('gift', 'Gift'),
+        ('trade', 'Trade'),
+        ('credit', 'Credit'),
+        ('stock_market', 'Stock Market'),
     ]
 
-    ACCOUNT_STATUS_CHOICES = [
-        ('active', 'Active'),
-        ('blocked', 'Blocked'),
-        ('closed', 'Closed'),
+    TRANSACTION_TYPE_CHOICES = [
+        ('inbound_transfer', 'Inbound Transfer'),
+        ('outbound_transfer', 'Outbound Transfer'),
+        ('freezing', 'Freezing'),
+        ('refill', 'Refill'),
+        ('inbound_payment', 'Inbound Payment'),
+        ('outbound_payment', 'Outbound Payment'),
+        ('reward', 'Reward'),
+        ('credit_payment', 'Credit Payment'),
+        ('credit_percentage', 'Credit Percentage'),
+        ('dividends', 'Dividends'),
+        ('salary_inbound', 'Salary Inbound'),
+        ('salary_outbound', 'Salary Outbound'),
+    ]
+
+    STATUS_CHOICES = [
+        ('in_progress', 'In Progress'),
+        ('completed', 'Completed'),
+        ('rejected', 'Rejected'),
+        ('declined', 'Declined'),
+        ('refunded', 'Refunded'),
     ]
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    type = models.CharField(choices=ACCOUNT_TYPE_CHOICES, max_length=20)
-    serverId = models.CharField(max_length=100, null=True, blank=True)
-    userId = models.CharField(max_length=100)
-    status = models.CharField(choices=ACCOUNT_STATUS_CHOICES, max_length=20)
+    accountId = models.ForeignKey('UserAccount', on_delete=models.CASCADE)
+    trade_type = models.CharField(choices=TRADE_TYPE_CHOICES, max_length=20)
+    transaction_type = models.CharField(choices=TRANSACTION_TYPE_CHOICES, max_length=20)
+    amount = models.DecimalField(max_digits=12, decimal_places=2)
+    status = models.CharField(choices=STATUS_CHOICES, max_length=20)
 
     def __str__(self):
         return str(self.id)
